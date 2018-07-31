cf8533aff639b84cff9977f793d23b0d017b9e78
木島優大朗 <eddix.ibanez@outlook.jp>
2018-08-01 04:00:29 +0900

    バグ修正

--- resources/views/oppai.blade.php
+++ resources/views/oppai.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style src="{{ asset('css/oppai-defo.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('style/oppai-defo.css') }}">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
