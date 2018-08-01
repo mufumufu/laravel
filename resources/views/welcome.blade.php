cdbaaa0962a6110a55cf849bd8d62a9a73cc1f7d
木島優大朗 <eddix.ibanez@outlook.jp>
2018-08-02 04:35:18 +0900

    Dishgramに変更。oppaiは残した

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,78 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>oppai</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="/oppai">OPPAI</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<title>Dishgram</title>
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"/>
+<div class="flex-center position-ref full-height">
+  <div class="content">
+    <div class="title m-b-md">
+      Dishgram
+    </div>
+    <div class="links">
+      <a href="/oppai">OPPAI</a>
+      <a href="https://laracasts.com">timeline</a>
+      <a href="https://laravel-news.com">News</a>
+      <a href="https://laravel.com">about Laravel</a>
+      <a href="https://www.instagram.com/explore/tags/東京グルメ/">Instergram</a>
+    </div>
+  </div>
+</div>
+<style>* {
+  box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  html, body{
+    background-color:rgb(255, 255, 255);
+    color:rgb(99, 107, 111);
+    font-family:Raleway, sans-serif;
+    font-weight:100;
+    height:100vh;
+    margin-top:0px;
+    margin-right:0px;
+    margin-bottom:0px;
+    margin-left:0px;
+  }
+  .full-height{
+    height:100vh;
+  }
+  .flex-center{
+    align-items:center;
+    display:flex;
+    justify-content:center;
+  }
+  .position-ref{
+    position:relative;
+  }
+  .content{
+    text-align:center;
+  }
+  .title{
+    font-size:84px;
+  }
+  .links > a{
+    color:rgb(99, 107, 111);
+    padding-top:0px;
+    padding-right:25px;
+    padding-bottom:0px;
+    padding-left:25px;
+    font-size:12px;
+    font-weight:600;
+    letter-spacing:0.1rem;
+    text-decoration:none;
+    text-transform:uppercase;
+  }
+  .m-b-md{
+    margin-bottom:30px;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin-top:0px;
+    margin-right:0px;
+    margin-bottom:0px;
+    margin-left:0px;
+  }
+</style>
